0475d0283c68626128fa22515ed95be61cb9396b
anachronism <mhli@wpi.edu>
2017-12-08 22:39:28 -0500

    First object is getting tracked, now need to figure out how to initialize and restart trackers

--- Incorporating Object Tracking/real_time_object_detection.py
+++ Incorporating Object Tracking/real_time_object_detection.py
@@ -1,6 +1,9 @@
 # USAGE
 # python real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
 '''
+NOTE: NEEDS https://pypi.python.org/pypi/opencv-contrib-python/3.3.0.10#downloads
+		and https://pypi.python.org/pypi/opencv-python/3.3.0.10
+		That is, it needs an opencv with compiled trackers.
 TODO (MAX):
 
 '''
@@ -20,19 +23,21 @@ import sys
 # Object class.
 class DetectedObject:
 
-	def __init__(self, startX, startY, endX, endY, label):
+	def __init__(self, startX, startY, endX, endY, labelIDX):
 		global objIDCnt
+		global CLASSES
 		self.start = [startX,startY]
 		self.end = [endX,endY]
-		self.label = label
+		self.labelIDX = labelIDX
 		self.id = objIDCnt
 		print(self.id)
 		objIDCnt = objIDCnt + 1
 
 	# Returns false if labels aren't the same. 
 	#(Note, not comparing two of the same object, just with the detection) 
-	def compare(self, startX,startY,endX,endY,label, margin): 
-		if label != self.label:
+	def compare(self, startX,startY,endX,endY,label, margin):
+		#print(label)
+		if label != CLASSES[self.labelIDX]:
 			return False
 		else:
 			# Compress these if trying to be codespace efficient.
@@ -40,7 +45,9 @@ class DetectedObject:
 			delta_y_top = self.start[1] - startY
 			delta_x_bottom = self.end[0] - endX
 			delta_y_bottom = self.end[1] - endY 
-			max_off = max(delta_x_top,delta_y_top,delta_x_bottom, delta_y_bottom)
+			#print(delta_x_top)
+			#max_off = max(delta_x_top,delta_y_top,delta_x_bottom, delta_y_bottom)
+			max_off = abs(delta_x_top)
 			if max_off > margin:
 				return False
 			else:
@@ -56,7 +63,7 @@ ap.add_argument("-m", "--model", default="MobileNetSSD_deploy.caffemodel",
 	help="path to Caffe pre-trained model")
 ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak detections")
-ap.add_argument("-f", "--trackframes", default=1,
+ap.add_argument("-f", "--trackframes", default=10,
 	help="Number of frames to track between detection events")
 args = vars(ap.parse_args())
 
@@ -77,6 +84,10 @@ cntFrame = 0
 objBuffer = [] # Buffer to store identified
 margin = 20
 objIDCnt = 0
+trackerHasInit = 0
+
+# Options are Boosting, MIL, KCF, TLD, MedianFlow, and GOTURN.
+tracker = cv2.TrackerKCF_create()
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
@@ -89,10 +100,11 @@ fps = FPS().start()
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
-	if cntFrame == 0:
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
 
+	# Run detection algorithm.	
+	if cntFrame == 0:
 		# grab the frame dimensions and convert it to a blob
 		(h, w) = frame.shape[:2]
 		blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
@@ -125,19 +137,23 @@ while True:
 				# draw the prediction on the frame
 				label = "{}: {:.2f}%".format(CLASSES[idx],
 					confidence * 100)
-
+				label_in = "{}".format(CLASSES[idx])
 				# Check if object that is detected already exists in detection buffer.
 				### NOTE CURRENT METHOD DOESNT ACCOUNT FOR DETECTOR CHANGING ITS MIND
 				###  INCORPORATE REMOVAL OF OBJECTS FROM objBuffer
 				doesExist = False
 				for elt in objBuffer:
-					if elt.compare(startX,startY,endX,endY,label,margin):
+					if elt.compare(startX,startY,endX,endY,label_in,margin):
 						elt.start = [startX, startY]
 						elt.end = [endX, endY]
 						doesExist = True
 
 				if not doesExist:
-					objBuffer.append(DetectedObject(startX,startY, endX, endY, label))
+					objBuffer.append(DetectedObject(startX,startY, endX, endY, idx))
+					if objIDCnt == 1: # That is, if the just added object is 0
+						if trackerHasInit == 0:
+							tracker.init(frame,(startX,startY,endX,endY))
+							trackerHasInit = 1
 
 				cv2.rectangle(frame, (startX, startY), (endX, endY),
 					COLORS[idx], 2)
@@ -158,12 +174,38 @@ while True:
 
 	else: ### MAX: Still to implement, tracking frames goes here.
 		### Don't forget to call fps.update(), redraw, etc
-		pass 
+		for elt in objBuffer:
+			### NOTE: ONLY TRACKING FIRST BOX, NEED TO FIGURE OUT HOW TO ADD TRACKERS
+			if elt.id == 0: 
+				ok,box = tracker.update(frame)
+				startX = int(box[0])
+				startY = int(box[1])
+				endX = int(box[2])
+				endY = int(box[3])
+				elt.start=[startX,startY]
+				elt.end = [endX, endY]
+				### Draw box, make sure color is kept.
+				cv2.rectangle(frame, (startX, startY), (endX, endY),
+					COLORS[elt.labelIDX], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, CLASSES[elt.labelIDX], (startX, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[elt.labelIDX], 2)
 
+		# show the output frame
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key was pressed, break from the loop
+		if key == ord("q"):
+			break
+
+		# update the FPS counter
+		fps.update()
 
 	cntFrame = cntFrame + 1
 	if cntFrame >= int(args["trackframes"]):
 		cntFrame = 0
+
 
 # stop the timer and display FPS information
 fps.stop()

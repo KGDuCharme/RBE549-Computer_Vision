712915bc1e5bcc205494168858ea82637b07ac9e
KGDuCharme <33527209+KGDuCharme@users.noreply.github.com>
2018-04-04 15:56:22 -0400

    Folder Clean up
    Cleaning up the organization of the project

--- 3. Work/Code/Dataset/images/resize.m
+++ 3. Work/Code/Dataset/images/resize.m
@@ -1,9 +1,12 @@
+% What file are we changing? Put name here
 name = '61a-NRAX4IL._SL1500_.jpg'
 
+% Read file
 image = imread(name);
 
+% Change size
 new_image = imresize(image, 0.5);
-% imshow(new_image)
 
+% Rewrite file with new size
 imwrite(new_image,name);
 
